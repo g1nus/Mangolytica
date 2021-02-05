b150a5a145ed54394745a0b5b409d10f6126e834
g1nus <giuliano.andronic@studenti.unitn.it>
2021-02-05 13:32:04 +0000

    basic search results

--- src/components/pages/search.js
+++ src/components/pages/search.js
@@ -1,13 +1,25 @@
 import React from 'react';
+import { useForm } from 'react-hook-form';
+import { useHistory } from "react-router-dom";
+
 import TrendingStreamers from 'components/modules/search/trendingStreamers';
 import SearchButton from 'components/svg/searchButton';
 
 const Search = function(props) {
+
+  let history = useHistory();
+
+  const { register, handleSubmit, watch, errors } = useForm();
+  const onSubmit = async function (data) {
+    console.log(`submitting`, data);
+    history.push(`/search_res?query=${data.query}`);
+  }
+
   return (
     <div id='search-wrapper'>
       <img src='/mangologo.png' id='logo' />
-      <form id='search-form'>
-        <input type='text' placeholder='search for a streamer...' />
+      <form id='search-form' onSubmit={handleSubmit(onSubmit)}>
+        <input type='text' name='query' placeholder='search for a streamer...' ref={register({ required: true })}/>
         <button type='submit'>
           <SearchButton />
         </button>
--- src/components/pages/searchResults.js
+++ src/components/pages/searchResults.js
@@ -1,45 +1,39 @@
 import React, {useEffect, useState, useRef} from 'react';
-import { useForm } from 'react-hook-form';
 
 import {searchDao} from 'dao/search.dao';
 
 const SearchResults = function(props) {
 
   const [searchResults, setSearchResults] = useState([]);
-  const [loading, setLoading] = useState(false);
 
-  const { register, handleSubmit, watch, errors } = useForm();
 
   //refernce and mount cycle handler
   const isMounted = useRef(true)
   useEffect(() => {
+    const query = window.location.hash.substring(window.location.hash.indexOf('query=') + 6).match(/[^(?|&|=)]*/)[0];
+
+    const fetchData = async () => {
+      try{
+        let results = await searchDao.searchStreamer(query);
+        console.log(results.data);
+        if(isMounted.current) setSearchResults(results.data);
+      }catch (err){
+        console.log(err);
+        if(isMounted.current) setSearchResults([{error: `failed`}]);
+      }
+    }
+
+    fetchData();
     return () => {
       isMounted.current = false
-      searchDao.cancelRequest();
     }
   }, []) 
 
-  const onSubmit = async function (data) {
-    try{
-      setLoading(true);
-      let results = await searchDao.searchStreamer(data.query);
-      setLoading(false);
-      console.log(results.data);
-      if(isMounted.current) setSearchResults(results.data);
-    }catch (err){
-      if(isMounted.current) setSearchResults([{error: `failed`}]);
-    }
-  }
 
   return (
     <>
       search page
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <input name="query" ref={register({ required: true })} />
-        {errors.query && <span>This field is required</span>}
-        <input type='submit'/>
-      </form>
-      {(loading) ?
+      {(searchResults.length === 0) ?
         "loading..."
       :
         <>
