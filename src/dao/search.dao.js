142ebf4e7cc894a36cc987a317eeb07ce2ac3618
g1nus <giuliano.andronic@studenti.unitn.it>
2021-02-07 08:03:38 +0000

    add fake dao for platform streamers

--- src/dao/search.dao.js
+++ src/dao/search.dao.js
@@ -4,7 +4,7 @@ import config from 'config';
 
 const source = axios.CancelToken.source();
 
-async function searchStreamer(query) {
+async function searchStreamerOnLine(query) {
   try {
     let resp = await axios.get(`${config.apiEndpoint}search?query=${query}&key=${config.apiSecret}`, {cancelToken: source.token});
     return resp.data;
@@ -13,12 +13,28 @@ async function searchStreamer(query) {
   }
 }
 
+async function searchStreamerOnMango(query) {
+  return new Promise(function (resolve, reject) {
+    setTimeout(() => {
+      resolve({data:[
+        {name: '_izio_caio', 
+          img_url:'http://placekitten.com/400/400',
+          twitch: {username: 'User_1', page: 'https://twitch.tv'}, twitter: {username: '@tweetlady', page: 'https://twitter.com'}, createdAt: new Date()},
+        {name: 'ciccio pasticcio', 
+          img_url: 'http://placekitten.com/200/200',
+          twitch: {username: 'User_2', page: 'https://twitch.tv'}, twitter: {username: '@tweetguy', page: 'https://twitter.com'}, createdAt: new Date()}
+      ]});
+    }, 2000);
+  });
+}
+
 function cancelRequest () {
   source.cancel(`[S]request cancelled`);
 }
 
 const searchDao = {
-  searchStreamer,
+  searchStreamerOnLine,
+  searchStreamerOnMango,
   cancelRequest
 }
 
