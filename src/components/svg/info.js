ede726ca53cb31068fd6767738b10bda1664d179
g1nus <giuliano.andronic@studenti.unitn.it>
2021-02-23 09:59:46 +0000

    styling fixes

--- src/components/svg/info.js
+++ src/components/svg/info.js
@@ -6,9 +6,9 @@ const Info = function (props) {
       <svg className="info-icon" xmlns="http://www.w3.org/2000/svg"
           xmlnsXlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
           viewBox="0 0 1000 1000">
-        <circle class="st0-info-icon" cx="500" cy="500" r="467"/>
-        <line class="st1-info-icon" x1="500" y1="406.2" x2="500" y2="837.1"/>
-        <line class="st1-info-icon" x1="500" y1="208.3" x2="500" y2="237.1"/>
+        <circle className="st0-info-icon" cx="500" cy="500" r="467"/>
+        <line className="st1-info-icon" x1="500" y1="406.2" x2="500" y2="837.1"/>
+        <line className="st1-info-icon" x1="500" y1="208.3" x2="500" y2="237.1"/>
 
 
      </svg>
